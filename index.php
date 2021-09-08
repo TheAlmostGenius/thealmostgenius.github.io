424e54288e183d782f59e5cb8e0dd346e4308dfa
John Wolfe <johnwolfe001@hotmail.co.uk>
2021-09-08 16:42:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "Hello World!";
 ?>
+
+</body>
+</html>
